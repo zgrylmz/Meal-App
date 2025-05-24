05140bfb8dd5c731150f90a1cc1710e2fc79ce78
Özgür <59776806+zgrylmz@users.noreply.github.com>
2025-05-25 01:50:15 +0200

    Update ingredientSlice.jsx

--- meal-app-frontend/src/Redux/recipeSlice/ingredientSlice.jsx
+++ meal-app-frontend/src/Redux/recipeSlice/ingredientSlice.jsx
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const getRecipesByIngredient = createAsyncThunk("getRecipesByIngredient",async({ingredientName})=>{
+export const getRecipesByIngredient = createAsyncThunk("getRecipesByIngredient",async(ingredientName)=>{
 
     const response = await axios.post(`${import.meta.env.VITE_API_URL}/getRecipeByIngredient`,{ingredientName},
         {withCredentials:true});
