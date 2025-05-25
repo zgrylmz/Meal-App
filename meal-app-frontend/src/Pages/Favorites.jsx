d0b77cd16cd5f30016fbf99556ee299f599d8ad6
zgrylmz <ozgurylmz@outlook.com>
2025-05-25 02:17:08 +0200

    improvement

--- meal-app-frontend/src/Pages/Favorites.jsx
+++ meal-app-frontend/src/Pages/Favorites.jsx
@@ -29,10 +29,10 @@ function Favorites() {
     const fetchFavorites = async () => {
       try {
         if (!userId && storedUserId) {
-          await dispatch(setUserId(storedUserId)).unwrap();
+          await dispatch(setUserId({storedUserId}));
           await dispatch(callFavoriteRecipes({ userId: storedUserId })).unwrap();
         } else if (userId) {
-          await dispatch(callFavoriteRecipes({ userId })).unwrap();
+          await dispatch(callFavoriteRecipes( {userId} )).unwrap();
         }
       } catch (err) {
         console.error('Error loading favorites:', err);
